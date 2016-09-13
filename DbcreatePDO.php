dc7769eaf415a219c7534e775833366a4a8c0fed
Mayandi <prabumaya.m317@gmail.com>
2016-09-13 12:22:36 +0800

    PDO db creation script

---
+++ DbcreatePDO.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* This code connect the database using PDO and create the database named "test"
+* @author Original Author <prabumaya.m3@gmail.com>
+* $modified on Sep-13-2016
+*/
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE test";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Database created successfully<br>";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
