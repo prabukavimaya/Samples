8b9b01fa8f6c9aba8281d35e6b1534c61d1b0c50
Mayandi <prabumaya.m317@gmail.com>
2016-09-13 12:16:29 +0800

    PDO db connection script

---
+++ DbConnectPdo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* This code connect the database using PDO
+* @author Original Author <prabumaya.m3@gmail.com>
+* $modified on Sep-13-2016
+*/
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
